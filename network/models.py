9205a3c796006cd770e6acbe3e80d315dead7144
n1ki4 <glazunov_n@ukr.net>
2020-03-28 14:05:02 +0200

    basic models and views

--- network/models.py
+++ network/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    added = models.DateTimeField(auto_now_add=True)
+
+    liked = models.IntegerField(default=0)
+    unliked = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
