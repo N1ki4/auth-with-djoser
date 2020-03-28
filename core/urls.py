f17eb948769820216fcd4f157788b8b9a35a767b
n1ki4 <glazunov_n@ukr.net>
2020-03-28 17:50:50 +0200

    implement json web tokens

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('network.urls')),
+
+    # Rest framework url
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # JWT Endpoints
+    path('auth-jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth-jwt-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- network/urls.py
+++ network/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostList, PostDetail
 
 urlpatterns = [
-    path('<int:pk/>', PostDetail.as_view()),
-    path('', PostList.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('', PostList.as_view(), name='posts_list'),
 ]
